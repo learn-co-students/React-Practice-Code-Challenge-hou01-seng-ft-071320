9b036c8b4b508dbdcfd4dc8167780169e6d1d99a
JuanToanXoup <Johndoan42@gmail.com>
2020-09-23 15:54:45 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -2,19 +2,68 @@ import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
 
-// Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [],
+    fourSushis: [],
+    selectedSushis: [],
+    moneyLeft: 100
+  }
+
+  componentDidMount(){
+    this.fetchSushi()
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer 
+          fourSushis={this.state.fourSushis}
+          eatSushi={this.eatSushi}
+          getMore={this.getMore}
+          selectedSushis={this.state.selectedSushis}
+        />
+        <Table 
+          selectedSushis={this.state.selectedSushis}
+          moneyLeft={this.state.moneyLeft}
+        />
       </div>
     );
   }
+
+  fetchSushi = ()=>{
+    fetch(API)
+    .then(res=>res.json())
+    .then(sushis => {
+      this.setState({
+        sushis: sushis,
+        fourSushis: sushis.sort(() => Math.random() - Math.random()).slice(0, 4)
+      })
+    })
+  }
+
+  eatSushi = (newSushi)=>{
+    const newMoneyLeft = this.state.moneyLeft - newSushi.price
+    if(newMoneyLeft >= 0){
+      let eatenSushi = [...this.state.selectedSushis]
+      eatenSushi.push(newSushi)
+      this.setState({
+        selectedSushis: eatenSushi,
+        moneyLeft: newMoneyLeft
+      })
+    }
+  }
+
+  getMore = ()=>{
+    let onlyNew = this.state.sushis.filter(sushi => !this.state.selectedSushis.includes(sushi))
+    this.setState({
+      fourSushis: onlyNew.sort(() => Math.random() - Math.random()).slice(0, 4)
+    })
+  }
+
 }
 
 export default App;
